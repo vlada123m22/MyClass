fd32782018985d46f55b4427071201ed08adc272
Vladislava Musin <vlada.musin@gmail.com>
2023-09-12 21:16:25 +0300

    Hope that it is the final commitment

---
+++ src/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+    private float speed = 0;
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void stop() {
+        speed=0;
+    }
+
+    public void accelerate(float amount) {
+        speed += amount;
+    }
+
+    public void printIsMoving() {
+        if (speed!=0) {
+            System.out.println("The car is moving");
+        } else {
+            System.out.println("The car is not moving");
+        }
+    }
+}
